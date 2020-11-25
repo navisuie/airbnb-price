350fda4c1ab79189d9112b0532e890e1b61df781
Jacob Lorenz <58476572+TheExplorativeBadger@users.noreply.github.com>
2020-11-24 18:34:51 -0600

    updating frontend

--- jlorenz2_front_end_developer_2/src/frontend/script.js
+++ jlorenz2_front_end_developer_2/src/frontend/script.js
@@ -1,27 +1,101 @@
-GetFilteredResults = function() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("demo").innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("GET", "url", true);
-    xhttp.send();
-}
+$(document).ready(function() {
 
-PostFilters = function() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("Listing_Summaries_List").innerHTML = this.responseText;
-        }
-    };
+    // ------------- General AJAX Handlers ------------------
+    // ------------------------------------------------------
 
-    var xmlHttpBody = {
+    /**
+     * A generic AJAX handler for all outgoing POST requests
+     * @param {String} pathUrl The url to send the request to
+     * @param {String} bodyData The stringified JSON request body
+     * @param {Function} successCallback The callback function to call on a successful response
+     */
+    function SendPostAJAXRequest(pathUrl, bodyData, successCallback) {
+        
+        $.ajax({
+            type: 'post',
+            url: pathUrl,
+            dataType: 'json',
+            contentType: "application/json; charset=utf-8",
+            data: bodyData,
+            success: successCallback
+        });
 
-    };
+    }
+
+    /**
+     * A generic AJAX handler for all outgoing GET requests
+     * @param {String} pathUrl The url to send the request to
+     * @param {Function} successCallback The callback function to call on a successful response
+     */
+    function SendGetAJAXRequest(pathUrl, successCallback) {
+        $.ajax({
+            type: 'get',
+            url: pathUrl,
+            dataType: 'json',
+            contentType: "application/json; charset=utf-8",
+            success: successCallback
+        });
+    }
+
+    // ----------- End General AJAX Handlers ----------------
+    // ------------------------------------------------------
+
+    // --------- Business Logic HTTP Requests ---------------
+    // ------------------------------------------------------
 
 
-    xhttp.open("POST", "/filteredResults", true);
-    xhttp.send(JSON.stringify(xmlHttpBody));
-}
+
+    
+
+    // --------- End Business Logic HTTP Requests -----------
+    // ------------------------------------------------------
+
+    // ----------- Callback Handler Functions ---------------
+    // ------------------------------------------------------
+
+
+
+
+
+
+
+
+    // --------- End Callback Handler Functions -------------
+    // ------------------------------------------------------
+
+    // ---------------------- Main --------------------------
+    // ------------------------------------------------------
+
+});
+
+
+
+
+
+// GetFilteredResults = function() {
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             document.getElementById("demo").innerHTML = this.responseText;
+//         }
+//     };
+//     xhttp.open("GET", "url", true);
+//     xhttp.send();
+// }
+
+// PostFilters = function() {
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             document.getElementById("Listing_Summaries_List").innerHTML = this.responseText;
+//         }
+//     };
+
+//     var xmlHttpBody = {
+
+//     };
+
+
+//     xhttp.open("POST", "/filteredResults", true);
+//     xhttp.send(JSON.stringify(xmlHttpBody));
+// }
